abdba851f3dbbf43cec342a3d1d425b341b96153
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 10:02:52 +0000

    Update Game.js

--- Game.js
+++ Game.js
@@ -2,11 +2,19 @@
 
 class Game {
   constructor () {
-    this.board: ;
-    this.players: ;
-    this.ready: ;
+    this.board: new Board();
+    this.players: this.createPlayers();
+    this.ready: false;
   }
-  createPlayers() {
   
-  }
+  /** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+  
+  createPlayers() {
+    const players = new Player('Player 1', 1, '#e15258', true),
+                    new Player('Player 2', 2,'#e59a13')];
+    return players;
+    }    
 }
