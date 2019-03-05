1dabe794c13f58827e7e0aff828e255cac412324
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 10:13:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+//Initialize new Game object
+
+new game();
 
 
 /** 
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
+$('.begin-game').eventListener('click', function() {
+  game.startGame();
+  this.style.display = 'none';
+  document.getElementById('play-area').style.opacity = '1';
+});
+
+
