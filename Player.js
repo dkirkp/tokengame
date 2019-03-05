962734926840122a427f6636a10d09726c4eb29b
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 08:43:42 +0000

    Create Player.js

---
+++ Player.js
@@ -0,0 +1,11 @@
+//Player Class file
+
+class Player {
+  constructor (name, id, active, color, tokens) {
+    this.name: name,
+    this.id: id,
+    this.active:false,
+    this.color: color,
+    this.token: []
+  }
+}
