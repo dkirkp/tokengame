f280b6212889f7036eca29031e97f0756710ac93
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 10:48:03 +0000

    Update Token.js

--- Token.js
+++ Token.js
@@ -6,4 +6,17 @@ class Token {
     this.id: `token`-${index}-${owner.id},
     this.dropped: false
   }
+  
+  // Create new html container for @this token
+  
+  drawHTMLToken() {
+    const tokenContainer = new (`<div></div>`)
+    document.getElementById('game-board-underlay').append(tokenContainer).setAttribute('id: this.id').setAttribute('class: token', 'backgroundColor: parent.Node.backgroundColor');
+  };
+  
+  // Get @method to return the HTML token element associated with the Token object
+  
+  get tokenContainer(){
+	return tokenContainer;
+}
 }
