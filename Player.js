2362014d7d668098e1efb517f4f3f0bfe717d23a
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 09:18:49 +0000

    Update Player.js

--- Player.js
+++ Player.js
@@ -6,6 +6,22 @@ class Player {
     this.id: id,
     this.active:false,
     this.color: color,
-    this.token: []
+    this.tokens: this.createTokens(21);
+  }
+  //Token creator method
+  /**
+ * Creates token objects for player
+ * @param     {number}    num - Number of token objects to be created
+ * @returns   {Array}     An array of the newly created token objects
+ */
+  
+  createTokens (num) {
+    const tokens = [];
+    
+    for (li i = 0; i< num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+     }
+    return tokens;
   }
 }
