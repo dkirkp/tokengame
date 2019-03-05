f692432d05faf1590fcd0a59866b56cfaca5e1ac
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 18:51:59 +0000

    Update Token.js

--- Token.js
+++ Token.js
@@ -2,9 +2,10 @@
 
 class Token {
   constructor (){
-    this.owner: owner,
-    this.id: `token`-${index}-${owner.id},
-    this.dropped: false
+    this.owner: owner;
+    this.id: `token`-${index}-${owner.id};
+    this.dropped: false;
+    this.columnLocation: 0;
   }
   
   // Create new html container for @this token
@@ -18,8 +19,36 @@ class Token {
   };
   
   // Get @method to return the HTML token element associated with the Token object
-  
-  get tokenContainer(){
-	return tokenContainer;
-}
+   
+ get offsetLeft() {
+	 return this.htmlToken.offsetLeft;
+ }
+	 moveLeft() {
+         if (this.columnLocation > 0) {
+         this.htmlToken.style.left = this.offsetLeft - 76;
+         this.columnLocation -= 1;
+          }
+       }
+	/** 
+       * Moves html token one column to right.
+     @param   {number}    columns - number of columns in the game board
+       */
+        moveRight(columns) {
+         if (this.columnLocation < columns -1) {
+         this.htmlToken.style.left = this.offsetLeft + 76;
+         this.columnLocation += 1;
+	 }
+	}
+	 /** 
+	 * Drops html token into targeted board space.
+	 * @param   {Object}   target - Targeted space for dropped token.
+	 * @param   {function} reset  - The reset function to call after the drop animation has completed.
+	 */
+	drop(target, reset) {
+		this.dropped = true;
+		
+		$(this.htmlToken).animate({
+		top: (target.y * target.diameter)
+		}, 750, 'easeOutBounce', reset);
+	}
 }
