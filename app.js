650a3781f18b5cfbad132c08d093596f7ad4229c
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 10:18:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const game = new Game();
 /** 
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
-$('.begin-game').eventListener('click', function() {
+document.getElementById('begin-game').addEventListener('click', function() {
   game.startGame();
   this.style.display = 'none';
   document.getElementById('play-area').style.opacity = '1';
