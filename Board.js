dcc478546f4f43a53e1b2f1136d7c7826177ee74
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 12:00:13 +0000

    Update Board.js

--- Board.js
+++ Board.js
@@ -29,9 +29,10 @@ class Board {
   //Create new board @method
 
   drawHTMLBoard() {
-    for (li i = 0; i < this.spaces.length; i++) {
-     li board = new Spaces(this.spaces[i]); 
+    for (let column of this.spaces) {
+      for (let space of column) {
+        space.drawSVGSpace();
+      }
     }
-    return board;
   };
 }
