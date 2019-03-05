4326b3846f98451a85107fe95cab51ba2bbce6f3
dkirkp <47711656+dkirkp@users.noreply.github.com>
2019-03-05 09:45:04 +0000

    Update Board.js

--- Board.js
+++ Board.js
@@ -4,7 +4,26 @@ class Board {
   constructor() {
     this.rows: 6;
     this.columns: 7;
-    this.spaces: [];
+    this.spaces: this.createSpaces();
   }
+  
+   /** 
+   * Generates 2D array of spaces. 
+   * @return  {Array}     An array of space objects
+   */
 
+  createSpaces () {
+    const spaces = [];
+    
+    for (li i = 0; i< this.columns; i++) {
+      const column = [];
+      
+      for (li j = 0; i < this.rows; j++) {
+       const space = new Spaces(i, j); 
+       column.push(space);
+      }
+     spaces.push(column);
+    }
+     return spaces;
+  }
 }
